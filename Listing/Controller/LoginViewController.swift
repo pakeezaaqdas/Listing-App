b3f7942b3b9dbda2fce3b37e75a75bfc4b6a86c4
Pakeeza Aqdas <pakeeza.aqdas@gmail.com>
2021-09-07 13:07:31 +0500

    Login and register UI created

---
+++ Listing/Controller/LoginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginViewController.swift
+//  Listing
+//
+//  Created by Pakeeza Aqdas on 07/09/2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func registerNowPressed(_ sender: UIButton) {
+        
+//        let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+
+//
+//        let registerVC = RegisterViewController()
+//        navigationController?.pushViewController(registerVC, animated: true)
+    }
+    @IBAction func loginContinuePressed(_ sender: UIButton) {
+//        let homeVC = ViewController()
+//        navigationController?.pushViewController(homeVC, animated: true)
+    }
+}
---
+++ Listing/Controller/RegisterViewController.swift
@@ -0,0 +1,25 @@
+//
+//  RegisterViewController.swift
+//  Listing
+//
+//  Created by Pakeeza Aqdas on 07/09/2021.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func loginPressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func registerContinuePressed(_ sender: UIButton) {
+    }
+    
+
+}
