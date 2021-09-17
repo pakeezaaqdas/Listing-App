df4dc597d6081c26d49e2872ec1622d4ff942a6d
Pakeeza Aqdas <pakeeza.aqdas@gmail.com>
2021-09-17 16:21:28 +0500

    23.1% at 4:19

--- Listing Tests/EditProfileViewControllerTest.swift
+++ Listing Tests/EditProfileViewControllerTest.swift
@@ -10,7 +10,7 @@ import XCTest
 
 class EditProfileViewControllerTest: XCTestCase {
 
-    var sut: LoginViewController!
+    var sut: EditProfileViewController!
 
     override func setUpWithError() throws {
         
@@ -18,9 +18,11 @@ class EditProfileViewControllerTest: XCTestCase {
         
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
            
-        sut = storyboard.instantiateViewController(withIdentifier: "Login") as? LoginViewController
+        sut = storyboard.instantiateViewController(withIdentifier: "editProfile") as? EditProfileViewController
         sut.loadViewIfNeeded()
- 
+        
+        sut.nameTextField.text = "Test"
+        sut.numberTextField.text = "03361234567"
     }
 
     override func tearDownWithError() throws {
@@ -29,6 +31,34 @@ class EditProfileViewControllerTest: XCTestCase {
         sut = nil
         
         try super.tearDownWithError()
+    }
+    
+    func testNameTextField_WhenCreated_HasNameContentType_And_AutoCapitalizationSet() throws {
+        let nameTextField = try XCTUnwrap(sut.nameTextField)
+        
+        XCTAssertEqual(nameTextField.textContentType, UITextContentType.name)
+        
+        XCTAssertEqual(nameTextField.autocapitalizationType, UITextAutocapitalizationType.words)
+        
+        XCTAssertNotEqual(nameTextField.text, "")
+    }
+    
+    func testPhoneNumberTextField_WhenCreated_HasTelephoneNumberContentType_And_PhonePadKeyboardTypeSet() throws {
+        let numberTextField = try XCTUnwrap(sut.numberTextField)
+        
+        XCTAssertEqual(numberTextField.textContentType, UITextContentType.telephoneNumber)
+        
+        XCTAssertEqual(numberTextField.keyboardType, UIKeyboardType.phonePad)
+        
+        XCTAssertNotEqual(numberTextField.text, "")
+
+    }
+    
+    //MARK: - view controller
+    
+    func testSaveBtnVerify()
+    {
+        XCTAssertNotNil(sut.saveButton)
     }
 
 }
